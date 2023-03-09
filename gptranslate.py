57f820830311d59a98279fab8d5412d35a0e163b
Nestor Demeure <nestordemeure@gmail.com>
2023-03-09 15:46:03 -0800

    tweak main

--- gptranslate.py
+++ gptranslate.py
@@ -20,13 +20,13 @@ print(f"Importing `{source_file}`...")
 book = epub.read_epub(source_file)
 
 # translating  the book
-print(f"translating `{source_file}`...")
+print(f"Translating `{source_file}`...")
 #book = translate_book(book, source_language=source_language, target_language=target_language, 
 #                      output_file=target_file, verbose=verbose)
-
 book = translate_book_parallel(book, source_language=source_language, target_language=target_language, 
                                output_file=target_file, verbose=verbose)
 
 # exporting the epub file
+print(f"Exporting `{target_file}`...")
 epub.write_epub(target_file, book)
 print(f"Done!")
